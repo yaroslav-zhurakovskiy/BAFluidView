9167ed3e1039a18d3160c7c66270be3be4cc266f
Yaroslav Zhurakovskiy <yaroslav.zhurakovskiy@gmail.com>
2019-12-09 16:10:47 +0200

    Add SWPM support

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "BAFluidView",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(
+            name: "BAFluidView",
+            targets: ["BAFluidView"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "BAFluidView",
+            dependencies: [],
+            path: "Pod/Classes"
+        )
+    ]
+)
